530d75e9406e5e158fff017dbb660a34e1773e74
Uncle-Ma11 <1933511939@qq.com>
2023-01-26 15:49:03 -0500

    read cli input

--- DnsClient.py
+++ DnsClient.py
@@ -1,6 +1,27 @@
-import sys, getopt
+import argparse
+import sys
 
-from socket import *
 
-def read_input(argv):
-    opts, args = getopt.getopt(argv, "")
+def read_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", type=int, dest="timeout", default="5", help="set query timeout")
+    parser.add_argument("-r", type=int, dest="retry", default="3",
+                        help="maximum number of times to retransmit an unanswered query before giving up")
+    parser.add_argument("-p", type=int, dest="port", default="53", help="the UDP port number of the DNS server")
+    query_type_group = parser.add_mutually_exclusive_group()
+    query_type_group.add_argument("-mx", action="store_true")
+    query_type_group.add_argument("-ns", action="store_true")
+    parser.add_argument("name")
+    parser.add_argument("server")
+    try:
+        argv = parser.parse_args()
+        print(argv)
+        print(argv.name)
+        print(argv.server)
+    except argparse.ArgumentError:
+        print(
+            "The input query form is not correct. \n should use: python DnsClient.py [-t timeout] [-r max-retries] [-p port] [-mx|-ns] @server name")
+
+
+if __name__ == "__main__":
+    read_input()
